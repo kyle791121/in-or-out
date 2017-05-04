28297c85f85812bbbeba642cb9ee851811e4d045
Kyle Chan <kylechan@ip-10-11-16-193.ap-northeast-1.compute.internal>
2017-05-04 16:15:49 +0800

    asdjqoiwdj

--- src/flows/chatter.js
+++ src/flows/chatter.js
@@ -23,20 +23,20 @@ module.exports = (app) => {
   //   }
   // })
   //
-  // slapp.message('^(thanks|thank you)', ['mention', 'direct_message'], (msg) => {
-  //   msg.say(['You are welcome', 'Of course'])
-  // })
-  //
-  // slapp.message('good night|bye', ['mention', 'direct_message'], (msg) => {
-  //   msg.say(['Cheers :beers:', 'Bye', 'Goodbye', 'Adios'])
-  // })
-  //
-  // slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
-  //   // respond only 40% of the time
-  //   if (Math.random() < 0.4) {
-  //     msg.say([':wave:', ':pray:', ':raised_hands:'])
-  //   }
-  // })
+  slapp.message('^(thanks|thank you)', ['mention', 'direct_message'], (msg) => {
+    msg.say(['You are welcome', 'Of course'])
+  })
+
+  slapp.message('good night|bye', ['mention', 'direct_message'], (msg) => {
+    msg.say(['Cheers :beers:', 'Bye', 'Goodbye', 'Adios'])
+  })
+
+  slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
+    // respond only 40% of the time
+    if (Math.random() < 0.4) {
+      msg.say([':wave:', ':pray:', ':raised_hands:'])
+    }
+  })
 
   slapp.message('play',(msg, text, match1) => {
     let myMsg = {
