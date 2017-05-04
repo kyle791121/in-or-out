7b888455332d2d5b46bed9bb857cd144af018891
Kyle Chan <kylechan@ip-10-11-16-193.ap-northeast-1.compute.internal>
2017-05-04 18:23:27 +0800

    qwqdqd

--- src/flows/help.js
+++ src/flows/help.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
   let slapp = app.slapp
 
   let help = {
-    "text": "嗨，我是你專屬的 Room Service 管家 Steve :tophat: ，我能夠幫你加快會議效率，讓你提早下班唷 :heart: \n你可以試試看輸入以下的指令：\n`1` 查詢現在半小時內的空會議室\n`2` 查詢現在一小時內的空會議室\n `MMDD HHMM HHMM` 查詢指定時間的空會議室，範例： `0505 1300 1330` 。"
+    "text": "嗨，我是你專屬的會議助理 Qmi :information_desk_person: ，我能夠幫你加快會議效率，讓你提早下班唷 :heart: \n你可以試試看輸入以下的指令：\n`1` 查詢現在半小時內的空會議室\n`2` 查詢現在一小時內的空會議室\n `MMDD HHMM HHMM` 查詢指定時間的空會議室，範例： `0505 1300 1330` 。"
   }
 
   slapp.command('/inorout', /^\s*help\s*$/, (msg) => {
